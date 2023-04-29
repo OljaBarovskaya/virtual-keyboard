e291f4d7efa0436cd1a4e0630f0549cbe4734400
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-04-29 14:44:54 +0300

    init: start project virtual-keyboard; feat: create arr for textinput

---
+++ script.js
@@ -0,0 +1,9 @@
+let apostrophe, num1,num2,num3,num4,num5,num6,num7,num8,num9,num0,minus,plus,slash;
+[apostrophe, num1,num2,num3,num4,num5,num6,num7,num8,num9,num0,minus,plus,backslash] = ['<sup>~</sup> `', '<sup>!</sup> 1','<sup>@</sup> 2','<sup>#</sup> 3','<sup>$</sup> 4','<sup>%</sup> 5','<sup>^</sup> 6','<sup>&</sup> 7','<sup>*</sup> 8','<sup>(</sup> 9','<sup>)</sup> 0','<sup>_</sup> -','<sup>+</sup> =','<sup>|</sup> \\'];
+let enKeysArr = [
+                  [apostrophe, num1,num2,num3,num4,num5,num6,num7,num8,num9,num0,minus,plus,'Backspace'],
+                  ['Tab','Q','W','E','R','T','Y','U','I','O','P','[',']','backslash','DEL'],
+                  ['Caps Lock','A','S','D','F','G','H','J','K','L',';',"'",'ENTER'],
+                  ['Shift','\\','Z','X','C','V','B','N','M',',','.','/','','Shift'],
+                  ['Ctrl','Win','Alt','','Alt','Ctrl','','','']
+                ]
